dc61bad4ba54ffec362edc55c8bb9f58d8c999e6
Denis Mathieu <denismathieu1203@gmail.com>
2024-01-12 16:33:46 +0100

    yugioh card displayer

--- assets/script.js
+++ assets/script.js
@@ -23,6 +23,27 @@ fetchFile()
 
 let button = document.querySelector("button");
 let textInput = document.querySelector("#textInput");
+
+fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?")
+        .then((response) => response.json())
+        .then((json) => {
+            let randomNb = randomNumber();
+            json.data[randomNb].card_images.forEach(element => {
+                let img = document.createElement("img");
+                img.src = element.image_url;
+                document.querySelector("main").appendChild(img);
+            });
+            
+            
+        })
+        .catch((error) => {
+            let para = document.createElement("p");
+            let paraContent = document.createTextNode("There was an error " + error);
+            para.appendChild(paraContent);
+            document.querySelector("main").appendChild(para);
+    
+        });
+
 button.addEventListener("click", (event) =>{
     event.preventDefault();
     if(document.querySelectorAll("img")){
@@ -30,19 +51,50 @@ button.addEventListener("click", (event) =>{
         images.forEach(elem =>{elem.remove();});
     }
     
-    if (textInput != ""){
+    if (textInput.value != ""){
     fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + textInput.value)
         .then((response) => response.json())
         .then((json) => {
             json.data[0].card_images.forEach(element => {
                 let img = document.createElement("img");
                 img.src = element.image_url;
-                document.querySelector("body").appendChild(img);
+                document.querySelector("main").appendChild(img);
             });
             
             
         })
+        .catch((error) => {
+            let para = document.createElement("p");
+            let paraContent = document.createTextNode("There was an error " + error);
+            para.appendChild(paraContent);
+            document.querySelector("main").appendChild(para);
+    
+        });
         textInput.value = "";
+    } else {
+        fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?")
+        .then((response) => response.json())
+        .then((json) => {
+            let randomNb = randomNumber();
+            json.data[randomNb].card_images.forEach(element => {
+                let img = document.createElement("img");
+                img.src = element.image_url;
+                document.querySelector("main").appendChild(img);
+            });
+            
+            
+        })
+        .catch((error) => {
+            let para = document.createElement("p");
+            let paraContent = document.createTextNode("There was an error " + error);
+            para.appendChild(paraContent);
+            document.querySelector("main").appendChild(para);
+    
+        });
     }
 
 });
+
+function randomNumber(){
+    return Math.floor(Math.random() * 12962);
+}
